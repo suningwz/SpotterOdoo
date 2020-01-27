37d485c76f89ffa6954b17de723d680e7e6b768b
juliancritchfieldSRF <58190936+juliancritchfieldSRF@users.noreply.github.com>
2020-01-27 13:27:22 -0700

    Restored orginal pricelist module
    Restored orginal pricelist module and then replaced product_product.py in models with error logging module from vendor.

---
+++ AddOns/filter_product_based_pricelist/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of CorTex IT Solutions Ltd.. See LICENSE file for full copyright and licensing details.
+
+from . import models
---
+++ AddOns/filter_product_based_pricelist/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of CorTex IT Solutions Ltd.. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Filter Products Based on Pricelist",
+    'version': "13.0.1.1.0",
+    'summary': "Filter products based on selected pricelist in Sale Order",
+    'category': 'Sale',
+    'description': """
+         This module allows you to select only those products which are assigned to the selected pricelist in Sale Order (SO).
+         Sale order filter product
+         filter products in sale order
+         filter products based on pricelist 
+    """,
+    'author': 'CorTex IT Solutions Ltd.',
+    'website': 'https://cortexsolutions.net',
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'price': 20,
+    'support': 'support@cortexsolutions.net',
+    'depends': ['sale_management', 'product'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'images': ['static/description/main_screenshot.gif'],
+}
