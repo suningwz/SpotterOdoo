fd3e7ade6200aa087f36402e4316a12fb3eec33a
camoore <cre.moore@bohmtech.com>
2020-12-21 15:31:12 -0600

    Add waiting status to repair orders

---
+++ AddOns/bohm_customizations/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bohm Customizations",
+    'summary': """Odoo customizations from Bohm Technologies""",
+    'description': """
+		Bohm Technologies Customizations
+	""",
+    'author': "Bohm Technologies",
+    'website': "https://www.bohmtechnologies.com",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['repair'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/repair_order_form.xml'
+    ],
+    'application': True,
+}
