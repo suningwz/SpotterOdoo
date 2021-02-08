648a9245e2a5f09c1b1c14fa6e8d79e6d1e99a7c
camoore <cre.moore@bohmtech.com>
2021-02-08 15:03:51 -0600

    add subscription info to serial model

--- AddOns/bohm_customizations/models/stock_production_lot.py
+++ AddOns/bohm_customizations/models/stock_production_lot.py
@@ -11,5 +11,17 @@ class CustomStockProductionLot(models.Model):
     _name = 'stock.production.lot'
     _inherit = 'stock.production.lot'
 
-
     x_warranty_date = fields.Datetime('Warranty Expiration Date')
+
+    x_linked_subscriptions = fields.Many2many(
+        'sale.subscription', string="Linked Subscriptions")
+
+    def update_subscriptions(self):
+        sale_orders = self.sale_order_ids
+
+        for sale in sale_orders:
+            lines = self.env['sale.order.line'].search(
+                [('order_id', '=', sale.id), ('subscription_id', '!=', False)])
+
+            for line in lines:
+                self.x_linked_subscriptions += line.subscription_id
