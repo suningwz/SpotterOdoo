3d622820037078bbcc2202a0dd40c917a986f380
camoore <cre.moore@bohmtech.com>
2021-02-09 13:18:00 -0600

    linked subscriptions/serials

--- AddOns/bohm_customizations/models/stock_production_lot.py
+++ AddOns/bohm_customizations/models/stock_production_lot.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import logging
-import datetime
 
 from odoo import _, api, fields, models
 
@@ -11,17 +10,8 @@ class CustomStockProductionLot(models.Model):
     _name = 'stock.production.lot'
     _inherit = 'stock.production.lot'
 
-    x_warranty_date = fields.Datetime('Warranty Expiration Date')
+    x_warranty_date = fields.Datetime('Warranty Expiration Date',
+                                      track_visibility='onchange')
 
     x_linked_subscriptions = fields.Many2many(
         'sale.subscription', string="Linked Subscriptions")
-
-    def update_subscriptions(self):
-        sale_orders = self.sale_order_ids
-
-        for sale in sale_orders:
-            lines = self.env['sale.order.line'].search(
-                [('order_id', '=', sale.id), ('subscription_id', '!=', False)])
-
-            for line in lines:
-                self.x_linked_subscriptions += line.subscription_id
