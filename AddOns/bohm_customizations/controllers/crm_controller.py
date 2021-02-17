379a9839d073ea8f9178f81888e18cf0d232516c
Cre Moore <cre.moore@bohmtech.com>
2021-02-17 14:50:44 -0600

    visibility of crm on portal

--- AddOns/bohm_customizations/controllers/crm_controller.py
+++ AddOns/bohm_customizations/controllers/crm_controller.py
@@ -5,18 +5,32 @@ class CustomWebsiteAccount(CustomerPortal):
 
     def get_domain_my_lead(self, user):
         return [
-            '|', '|',
+            '|', '|', '|', '|', '|', '|', '|', '|', '|', 
             ('x_studio_partner_1.id', '=', user.partner_id.id),
+            ('x_studio_partner_1', 'child_of', user.commercial_partner_id.id),
+            ('x_studio_consultant_1.id', '=', user.partner_id.id),
+            ('x_studio_consultant_1', 'child_of', user.commercial_partner_id.id),
+            ('x_studio_distributor.id', '=', user.partner_id.id),
+            ('x_studio_distributor', 'child_of', user.commercial_partner_id.id),
             ('x_studio_rep_firm.id', '=', user.partner_id.id),
-            ('partner_assigned_id', 'child_of', user.commercial_partner_id.id),
+            ('x_studio_rep_firm', 'child_of', user.commercial_partner_id.id),
+            ('partner_id.id', '=', user.partner_id.id),
+            ('partner_id', 'child_of', user.commercial_partner_id.id),
             ('type', '=', 'lead')
         ]
 
     def get_domain_my_opp(self, user):
         return [
-            '|', '|',
+            '|', '|', '|', '|', '|', '|', '|', '|', '|', 
             ('x_studio_partner_1.id', '=', user.partner_id.id),
+            ('x_studio_partner_1', 'child_of', user.commercial_partner_id.id),
+            ('x_studio_consultant_1.id', '=', user.partner_id.id),
+            ('x_studio_consultant_1', 'child_of', user.commercial_partner_id.id),
+            ('x_studio_distributor.id', '=', user.partner_id.id),
+            ('x_studio_distributor', 'child_of', user.commercial_partner_id.id),
             ('x_studio_rep_firm.id', '=', user.partner_id.id),
-            ('partner_assigned_id', 'child_of', user.commercial_partner_id.id),
+            ('x_studio_rep_firm', 'child_of', user.commercial_partner_id.id),
+            ('partner_id.id', '=', user.partner_id.id),
+            ('partner_id', 'child_of', user.commercial_partner_id.id),
             ('type', '=', 'opportunity')
         ]
